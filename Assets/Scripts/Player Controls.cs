bb8c4435206c4aacf54858398f92e83a3da69b52
maxkung101 <maxkung06@gmail.com>
2020-11-05 11:23:01 -0500

    update *
    Google VR plugins

---
+++ Assets/Scripts/Player Controls.cs
@@ -0,0 +1,101 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Player Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Player Controls"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""0edc44bd-717d-413a-a2ac-0f0bfd166f3c"",
+            ""actions"": [],
+            ""bindings"": []
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    public struct GameplayActions
+    {
+        private @PlayerControls m_Wrapper;
+        public GameplayActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    public interface IGameplayActions
+    {
+    }
+}
---
+++ Assets/Scripts/SkyView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class SkyView : MonoBehaviour
+{
+    public Material daytime, nighttime;
+
+    private DateTime moment;
+    private int hour;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        moment = DateTime.Now;
+        hour = moment.Hour;
+        if (hour >= 6 && hour < 18)
+        {
+            RenderSettings.skybox = daytime;
+        }
+        else
+        {
+            RenderSettings.skybox = nighttime;
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
