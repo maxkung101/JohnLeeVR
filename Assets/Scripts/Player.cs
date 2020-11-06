793c480bfa2046d46a3f15d8344aab9927f4d795
maxkung101 <maxkung06@gmail.com>
2020-11-06 11:39:38 -0500

    update *
    Updated main menu

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Player's room coordinates
+    public Vector3 mannten, moncher, sharon;
+
+    // Area coordinates
+    public Vector3 foyer, frontYard;
+
+    private int id;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        id = PlayerPrefs.GetInt("Player Id", 0);
+        ToStartingPosition();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void ToStartingPosition()
+    {
+        switch(id)
+        {
+            case 1:
+                transform.position = mannten;
+                break;
+            case 2:
+                transform.position = moncher;
+                break;
+            case 3:
+                transform.position = sharon;
+                break;
+            default:
+                transform.position = mannten;
+                break;
+        }
+    }
+
+    private void ToFoyer()
+    {
+        transform.position = foyer;
+    }
+
+    private void ToFrontYard()
+    {
+        transform.position = frontYard;
+    }
+}
